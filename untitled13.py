dc660038fb19cb8b5a1e8f73a9bd8cfc897ae713
Serhan Emre Telatar <telatarserhan@gmail.com>
2023-05-06 14:43:12 +0300

    Add files via upload

---
+++ untitled13.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+def intBudgetMaker(budgetStr):
+    
+    budgetStr = budgetStr.replace("$", "").replace(",", "").replace("(estimated)", "").strip()
+    budgetInt = int(budgetStr)
+    return budgetInt
+
+    
+URL = "https://www.imdb.com/list/ls056515174/?st_dt=&mode=simple&page=1&sort=user_rating,desc"
+
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"}
+
+page = requests.get(URL, headers=headers)
+
+content = bs(page.content, 'html.parser')
+
+counter= 0
+
+print("Name                                                Year     Rate     Budget          Link")
+print()
+for item in content.find_all('div', {'class': 'lister-item mode-simple'}):
+    link = "https://www.imdb.com/"+item.find('div', class_='lister-item-image').find('a')['href']+"?ref_=ttls_li_tt"
+
+    name = item.find('span', class_='lister-item-header').find('a').text.strip()
+    year = int(item.find('span', class_='lister-item-year').text[1:5])
+    rate = float(item.find('strong').text.strip())
+    
+    budgetPage = requests.get(link, headers=headers)
+    budgetPageContent = bs(budgetPage.content, 'html.parser')
+    try:
+        budget = budgetPageContent.find(attrs={"data-testid": "title-boxoffice-cumulativeworldwidegross"}).find('span', class_='ipc-metadata-list-item__list-content-item').text
+        counter +=1
+        
+        print(name, " "*(50-len(name)), year, " "*3, rate, " "*4, "$" , intBudgetMaker(budget)+1, " "*10, link)
+    except:
+        continue
+     
+print()
+print(counter)
